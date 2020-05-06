105cd7bdc2b6063a778be439b70218dc627d66d0
BlackFox <bigdeli.ali3@gmail.com>
2020-05-06 20:30:54 +0430

    Update db_to_excel.py

--- db_to_excel.py
+++ db_to_excel.py
@@ -8,9 +8,22 @@ class db_mgmt:
         self.conn = sqlite3.connect(self.file_path)
         self.c = self.conn.cursor()
 
-    def read(self,table_name):   
+    def tables_in_sqlite_db(self):
+        self.c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        table_names_list = self.c.fetchall()
+        return table_names_list
+    
+    def columns_in_table(self,table_name):
+        self.c.execute(f"select * from {table_name} where 1=0;")
+        return [d[0] for d in self.c.description]
+
+    def read(self,table_name):
+        rows = []
+        
+        rows.append(tuple(self.columns_in_table(table_name)))
         self.c.execute(f"SELECT * FROM {table_name}")
-        rows = self.c.fetchall()
+        rows_temp = self.c.fetchall()
+        rows += rows_temp
         return rows
 
     def close(self):
@@ -21,22 +34,24 @@ class xls_mgmt:
     def __init__(self,file_name="OUTPUT.xlsx"):
         self.file_path = file_name
         self.workbook = xlsxwriter.Workbook(file_name)
-        self.worksheet = self.workbook.add_worksheet() 
-    
-    def write_to_xlsx(self,data):
+        
+    def write_to_xlsx(self,table_name,data):
+        self.worksheet = self.workbook.add_worksheet(table_name) 
         for row in data:
             for cell in row:
-                    self.worksheet.write(int(data.index(row)),int(row.index(cell)),cell) 
-
+                    self.worksheet.write(int(data.index(row)),int(row.index(cell)),cell)  
+    
     def close(self):
         self.workbook.close() 
 
 
 if __name__ == "__main__":
-    db_name,table_name = input("enter database name and table name:\nexample: linkedin_db,linkedinposts\n:").split(",")
+    db_name = input("enter database name: ")
     db = db_mgmt(f"./{db_name}.db")
-    data = db.read(table_name)
     excel = xls_mgmt()
-    excel.write_to_xlsx(data)
+    tables = db.tables_in_sqlite_db()
+    for table in tables:
+        data = db.read(table[0])
+        excel.write_to_xlsx(table[0],data)
     excel.close()
 
